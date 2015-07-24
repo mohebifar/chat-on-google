8915a47eaab16cf163dd9c563b4fd59251e8a1eb
Mohamad Mohebifar <netfars@gmail.com>
2015-07-24 23:33:59 +0430

    Initial Commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/chatongoogle-dev'
+  },
+
+  seedDB: true
+};
