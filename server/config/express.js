6cb600878c9c03a92210ca81015819d1fcaba6b1
Mohamad Mohebifar <netfars@gmail.com>
2015-07-25 12:08:36 +0430

    fix: Fix static files loading issue on production env

--- server/config/express.js
+++ server/config/express.js
@@ -26,6 +26,7 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
+  app.use('/static', express.static(config.static));
 
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
@@ -38,7 +39,6 @@ module.exports = function(app) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
-    app.use('/static', express.static(path.join(config.root, 'static')));
     app.set('appPath', 'client');
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
